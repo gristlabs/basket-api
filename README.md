ef929ff09ed360e3bb3d29774bbb5acc048a52a9
Jake Streb <jake@getgrist.com>
2017-07-12 17:56:59 -0400

    version 1.0

--- README.md
+++ README.md
@@ -1,2 +1,66 @@
 # basket
-Standalone data storage and integration solution
+Standalone data solution useful for displaying and interacting with data on your website
+
+API
+
+All methods implement node-style callback functions or, if omitted, return ES6 Promises.
+The listed return values show what is passed to the callback or the Promise.
+
+```js
+/**
+ * Creates a Basket instance for interacting with a single basket.
+ * @param {String} basketId - Basket identifier string.
+ * @param {String} apiKey - API Key giving certain interaction permission. May be null if
+ *  not required.
+ */
+let basket = new Basket(basketId, apiKey)
+
+/**
+ * Adds a table to the basket
+ * @param {String} optTableId - Optional name for the new table. If omitted, a default 'Table1'
+ *  style name is used.
+ * @returns {String} - The name of the new table, which may be a sanitized version of the input.
+ */
+basket.addTable(optTableId, callback)
+
+/**
+ * Retrieves a table from the basket
+ * @param {String} tableId - The name of the table to retreive.
+ * @returns {Object} - The table data.
+ */
+basket.getTable(tableId, callback)
+
+/**
+ * Renames a table in the basket
+ * @param {String} oldTableId - The current name of the table to rename.
+ * @param {String} newTableId - The new name for the table.
+ * @returns {String} - The name of the new table, which may be a sanitized version of the input.
+ */
+basket.renameTable(oldTableId, newTableId, callback)
+
+/**
+ * Overwrites the current data in a table with new data
+ * @param {String} tableId - The name of the table whose data should be replaced.
+ * @param {Object} columnValues - The new data for the table.
+ * @returns null
+ */
+basket.replaceTableData(tableId, columnValues, callback)
+
+/**
+ * Deletes the table in the basket
+ * @param {String} tableId - The name of the table to be deleted.
+ * @returns null
+ */
+basket.deleteTable(tableId, callback)
+
+/**
+ * Lists tables in the basket
+ * @returns {Array} - An array of tableIds of tables in the basket
+ */
+ basket.getTables(callback)
+```
+
+index.js is produced by babel from basket.js via:
+```sh
+babel basket.js --watch --out-file index.js
+```
---
+++ index.js
@@ -0,0 +1,155 @@
+'use strict';
+
+var _Promise = typeof Promise === 'undefined' ? require('es6-promise').Promise : Promise;
+
+const HOST = "https://syvvdfor2a.execute-api.us-east-1.amazonaws.com";
+const BASE_PATH = "/test";
+
+/**
+ * Creates a Basket instance for interacting with a single basket.
+ * @param {String} basketId - Basket identifier string.
+ * @param {String} apiKey - API Key giving certain interaction permission. May be null if
+ *  not required.
+ */
+function Basket(basketId, apiKey) {
+  this.basketId = basketId;
+  this.apiKey = apiKey;
+}
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *                             Public class methods                                    *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+// Adds a table with tableId to the basket.
+Basket.prototype.addTable = function (optTableId, callback) {
+  // Since the table ID is optional, it may be the callback function.
+  if (!callback && typeof optTableId === 'function') {
+    callback = optTableId;
+    optTableId = null;
+  }
+  return request('POST', `/${this.basketId}/tables`, { tableId: optTableId }, { apiKey: this.apiKey }, callback);
+};
+
+// Retrieves all data from the table with tableId from the basket.
+Basket.prototype.getTable = function (tableId, callback) {
+  return request('GET', `/${this.basketId}/tables/${tableId}`, null, { apiKey: this.apiKey }, callback);
+};
+
+// Renames the table from oldTableId to newTableId.
+Basket.prototype.renameTable = function (oldTableId, newTableId, callback) {
+  return request('PUT', `/${this.dbasketId}/tables/${oldTableId}`, { tableId: newTableId }, { apiKey: this.apiKey }, callback);
+};
+
+// Replaces table with tableId data with columnValues.
+Basket.prototype.replaceTableData = function (tableId, columnValues, callback) {
+  return request('PUT', `/${this.basketId}/tables/${tableId}`, columnValues, { apiKey: this.apiKey }, callback);
+};
+
+// Deletes table with tableId from the basket.
+Basket.prototype.deleteTable = function (tableId, callback) {
+  return request('DELETE', `/${this.basketId}/tables/${tableId}`, null, { apiKey: this.apiKey }, callback);
+};
+
+// Returns a list of tableIds in the current basket.
+Basket.prototype.getTables = function (callback) {
+  return request('GET', `/${this.basketId}/tables`, null, { apiKey: this.apiKey }, callback);
+};
+
+// Basket.prototype.getRows = function(tableId, columnValues, callback) {
+
+// };
+
+// Basket.prototype.addRows = function(tableId, columnValues, callback) {
+
+// };
+
+// Basket.prototype.editRows = function(tableId, columnValues, callback) {
+
+// };
+
+// Basket.prototype.deleteRows = function(tableId, columnValues, callback) {
+
+// };
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *                  Private class methods (require authentication)                     *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+// Deletes a basket with the given basketId. Requires a login object.
+Basket.prototype.delete = function (login, callback) {
+  return request('DELETE', `/${this.basketId}`, null, { login: login }, callback);
+};
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *                 Private static methods (require authentication)                     *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+// Adds a basket belonging to the user. Requires a login object.
+Basket.addBasket = function (login, callback) {
+  return request('POST', `/`, null, { login: login }, callback);
+};
+
+// Returns an array of basketIds belonging to the user. Requires a login object.
+Basket.getBaskets = function (login, callback) {
+  return request('GET', `/`, null, { login: login }, callback);
+};
+
+/**
+ * Makes a request to the API Gateway controlling the Basket Lambda function.
+ * @param {String} method - The name of the HTTP method in all caps (ex: GET)
+ * @param {String} path - The resource path (ex: /b12345678/tables/Menu)
+ * @param {Object} body - The JSON body of the request
+ * @param {Object} options.apiKey - The key used to give permission to perform the method
+ *  on the resource path.
+ * @param {Object} options.login - The login object mapping from provider to token, which
+ *  should be provided for methods which require authorization.
+ * @param {Function} optCallback - The callback function of the form function(err, res) {}.
+ *  If omitted, the function returns an ES6 Promise.
+ */
+function request(method, path, body, options, optCallback) {
+  // If the callback is provided, use the standard callback function. Otherwise, use
+  //  the promisifed function.
+  if (optCallback) {
+    return _requestCallback(method, path, body, options, optCallback);
+  }
+  return new _Promise(function (resolve, reject) {
+    return _requestCallback(method, path, body, options, function (err, result) {
+      return err ? reject(err) : resolve(result);
+    });
+  });
+}
+
+// The request function, implemented with a callback.
+function _requestCallback(method, path, body, options, callback) {
+  options = options || {};
+  let xhr = new XMLHttpRequest();
+  let url = HOST + BASE_PATH + path;
+  xhr.open(method, url, true);
+  xhr.setRequestHeader('Content-Type', 'application/json');
+  if (options.login) {
+    xhr.setRequestHeader('Authorization', JSON.stringify(options.login));
+  } else if (options.apiKey) {
+    xhr.setRequestHeader('Authorization', options.apiKey);
+  }
+  xhr.onload = function () {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      let response = JSON.parse(xhr.responseText);
+      if (response.errorMessage) {
+        callback(new Error(response.errorMessage));
+      } else {
+        callback(null, response);
+      }
+    }
+    callback(new Error(xhr.statusText));
+  };
+  xhr.onerror = function () {
+    callback(new Error(xhr.statusText));
+  };
+  xhr.send(body ? JSON.stringify(body) : null);
+}
+
+if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+  module.exports = Basket;
+} else {
+  window.Basket = Basket;
+}
